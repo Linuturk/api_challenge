28b57027fc73855cf76ca8e5b0025760870c0bb7
Justin Phelps <linuturk@onitato.com>
2013-03-22 15:27:36 -0500

    Challenge 4 complete.

--- challenge4.py
+++ challenge4.py
@@ -25,5 +25,29 @@ parser.add_argument('FQDN', type=str,
                     help="The name of the new A record.")
 parser.add_argument('IP', type=str,
                     help="The IP address for this new record.")
+parser.add_argument('-z', '--zone', type=str,
+                    help="Specify the zone where the record is to be added.")
 
 args = parser.parse_args()
+
+# Handle the zone
+if args.zone:
+    zone = args.zone
+else:
+    # Give the (hopefully) root domain of the FQDN.
+    zone = '.'.join(args.FQDN.split('.')[-2:])
+
+# Build the a record dictionary
+a_record = {"type": "A",
+            "name": args.FQDN,
+            "data": args.IP,
+            "ttl": 300}
+
+# Check for the domain
+try:
+    dom = dns.find(name=zone)
+except pyrax.exceptions.NotFound:
+    logging.error("Domain %s was not found. Specify your zone using -z.", zone)
+else:
+    records = dom.add_records(a_record)
+    print records
