6e7f1fd63c27685f6e97dca0fb4e7d8b3778d9e2
Justin Phelps <linuturk@onitato.com>
2013-03-29 09:36:48 -0500

    Small typo.

--- challenge3.py
+++ challenge3.py
@@ -5,7 +5,8 @@ import logging
 import argparse
 import os.path
 
-challenge = """Write a script that accepts a directory as an argument as well
+challenge = """
+            Write a script that accepts a directory as an argument as well
             as a container name. The script should upload the contents of the
             specified directory to the container (or create it if it doesn't
             exist). The script should handle errors appropriately.
