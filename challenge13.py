aea39b4cc1e2033e99b28fb88f0dc8663865fb97
Justin Phelps <linuturk@onitato.com>
2013-04-20 19:09:58 -0500

    Final Challenge 13 added.

---
+++ challenge13.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import pyrax
+
+challenge = """
+            Write an application that nukes everything in your Cloud Account.
+            It should Delete All:
+             - Cloud Servers
+             - Custom Images
+             - Cloud Files Containers and Objects
+             - Databases
+             - Networks
+             - CBS Volumes
+            """
+points = 3
